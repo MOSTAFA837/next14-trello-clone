073b60943989799894a69c30a7359f5f6aab5721
MOSTAFA837 <mostafamanchester837@gmail.com>
2023-12-30 00:47:31 +0200

    mobile sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -57,6 +57,12 @@ export default function Sidebar({
           <Skeleton className="h-10 w-[50%]" />
           <Skeleton className="h-10 w-10" />
         </div>
+
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
